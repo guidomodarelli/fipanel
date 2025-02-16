54d4302a6b82e1067a137a35c85fc8152f5ccaac
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-16 15:31:16 +0000

    style: apply consistent formatting and minor code improvements across components

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // Create a client
 const queryClient = new QueryClient();
 
-const   Providers = ({ children }: React.PropsWithChildren) => {
+const Providers = ({ children }: React.PropsWithChildren) => {
   return (
     <QueryClientProvider client={queryClient}>
       <HeroUIProvider className='h-full'>{children}</HeroUIProvider>
