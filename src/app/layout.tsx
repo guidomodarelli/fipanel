4957fd7de7046ec7bf843776b7698715849f4b18
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-16 15:29:27 +0000

    feat: add Providers component and configure HeroUI and React Query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Providers from "@/providers/Providers";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className='px-8'>{children}</div>
+        <Providers>
+          <div className='px-8'>{children}</div>
+        </Providers>
       </body>
     </html>
   );
---
+++ src/providers/Providers.tsx
@@ -0,0 +1,15 @@
+import { HeroUIProvider } from '@heroui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create a client
+const queryClient = new QueryClient();
+
+const   Providers = ({ children }: React.PropsWithChildren) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HeroUIProvider className='h-full'>{children}</HeroUIProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default Providers;
