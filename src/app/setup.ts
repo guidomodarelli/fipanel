274f759496fa93056f3130a604e9d058fa389d58
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-17 00:19:34 +0000

    refactor: reorganize imports in setup.ts for improved clarity

--- src/app/setup.ts
+++ src/app/setup.ts
@@ -1,11 +1,11 @@
-import { AxiosHttpService } from '@/core/contexts/shared/http/infrastructure/AxiosHttpService';
-import { HttpService } from '@/core/contexts/shared/http/domain/HttpService';
 import { getPrincipalesVariablesUseCase } from '@/core/contexts/BCRA/application/getPrincipalesVariablesUseCase';
 import { HttpBCRA } from '@/core/contexts/BCRA/infrastructure/HttpBCRA';
 import { getDolarsUseCase } from '@/core/contexts/Dolar/application/getDolarsUseCase';
 import { HttpBluelyticsDolar } from '@/core/contexts/Dolar/infrastructure/HttpBluelyticsDolar';
-import { AlphavantageSymbolService } from '@/core/contexts/Alphavantage/infrastructure/AlphavantageSymbolService';
-import { getSymbolPriceMonthlyUseCase } from '@/core/contexts/Alphavantage/application/getSymbolPriceMonthlyUseCase';
+import { HttpService } from '@/core/contexts/shared/http/domain/HttpService';
+import { AxiosHttpService } from '@/core/contexts/shared/http/infrastructure/AxiosHttpService';
+import { getSymbolPriceMonthlyUseCase } from '@/core/contexts/Symbol/application/getSymbolPriceMonthlyUseCase';
+import { AlphavantageSymbolService } from '@/core/contexts/Symbol/infrastructure/AlphavantageSymbolService';
 
 const httpService: HttpService = new AxiosHttpService();
 const bcraService = new HttpBCRA(httpService);
