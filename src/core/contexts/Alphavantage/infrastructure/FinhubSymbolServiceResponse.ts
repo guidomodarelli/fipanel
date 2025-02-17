5ce9f0b94afdc9a6fd0a401bed655f0ba8a789a1
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-17 00:15:21 +0000

    feat: reorder properties in FinhubSymbolServiceResponse interface for clarity

--- src/core/contexts/Alphavantage/infrastructure/FinhubSymbolServiceResponse.ts
+++ src/core/contexts/Alphavantage/infrastructure/FinhubSymbolServiceResponse.ts
@@ -1,18 +1,18 @@
 export interface FinhubSymbolServiceResponse {
+  // Open price of the day
+  o: number;
+  // Previous close price
+  pc: number;
+  // High price of the day
+  h: number;
+  // Low price of the day
+  l: number;
   // Current price
   c: number;
   // Change
   d: number;
   // Percent change
   dp: number;
-  // High price of the day
-  h: number;
-  // Low price of the day
-  l: number;
-  // Open price of the day
-  o: number;
-  // Previous close price
-  pc: number;
   // Timestamp
   t: number;
 }
