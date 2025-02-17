e31f61504eb774da291f61d0665ca64ec1189a93
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-17 00:14:58 +0000

    feat: restore '4. close' property in SymbolMonthlyTimeSeries interface

--- src/core/contexts/Alphavantage/infrastructure/AlphavantageSymbolPriceMonthlyResponse.ts
+++ src/core/contexts/Alphavantage/infrastructure/AlphavantageSymbolPriceMonthlyResponse.ts
@@ -7,9 +7,9 @@ export interface SymbolMetaData {
 
 export interface SymbolMonthlyTimeSeries {
   '1. open': string;
+  '4. close': string;
   '2. high': string;
   '3. low': string;
-  '4. close': string;
   '5. volume': string;
 }
 
