abf079c3e59d5a962442e4b10c4bdeeebc6af996
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-19 01:33:23 +0000

    feat: add MONTH enum to represent months of the year

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,3 +1,18 @@
+export enum MONTH {
+  JANUARY = 0,
+  FEBRUARY = 1,
+  MARCH = 2,
+  APRIL = 3,
+  MAY = 4,
+  JUNE = 5,
+  JULY = 6,
+  AUGUST = 7,
+  SEPTEMBER = 8,
+  OCTOBER = 9,
+  NOVEMBER = 10,
+  DECEMBER = 11,
+}
+
 export const formatDate = (date: Date) => {
   return date.toLocaleDateString('es-AR', { day: 'numeric', month: 'numeric', year: 'numeric' });
 };
