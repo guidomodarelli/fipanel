6b38905ecab53fa1f5f2dd9eab6764531406a477
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-21 03:18:33 +0000

    feat: update MockSymbolProvider to support date range for daily and monthly price retrieval

--- src/core/contexts/Symbol/infrastructure/MockSymbolProvider.ts
+++ src/core/contexts/Symbol/infrastructure/MockSymbolProvider.ts
@@ -3,51 +3,42 @@ import { SymbolPriceInfo } from '../domain/SymbolPriceInfo';
 import { SymbolProvider } from '../domain/SymbolProvider';
 
 export class MockSymbolProvider implements SymbolProvider {
-  public async getSymbolPriceMonthly(symbol: string): Promise<SymbolPriceInfo[]> {
-    const mockData = this.generateMonthlyMockData();
-
-    return Promise.resolve(mockData);
+  public async getSymbolPriceMonthly(symbol: string, from: Date, to: Date): Promise<SymbolPriceInfo[]> {
+    return Promise.resolve(this.generateMonthlyMockData(from, to));
   }
 
-  public async getSymbolPriceDaily(symbol: string): Promise<SymbolPriceInfo[]> {
-    const mockData = this.generateDailyMockData();
-
-    return Promise.resolve(mockData);
+  public async getSymbolPriceDaily(symbol: string, from: Date, to: Date): Promise<SymbolPriceInfo[]> {
+    return Promise.resolve(this.generateDailyMockData(from, to));
   }
 
-  private generateDailyMockData(): SymbolPriceInfo[] {
-    const currentDate = new Date();
+  private generateDailyMockData(from: Date, to: Date): SymbolPriceInfo[] {
     const mockData: SymbolPriceInfo[] = [];
+    let currentDate = new Date(from);
 
-    for (let i = 0; i < 30; i++) {
-      const date = new Date();
-      date.setDate(currentDate.getDate() - i);
-
+    while (currentDate <= to) {
       // Check if the day is Saturday (6) or Sunday (0)
-      if (date.getDay() === 6 || date.getDay() === 0) {
-        continue;
+      if (currentDate.getDay() !== 6 && currentDate.getDay() !== 0) {
+        mockData.push({
+          open: parseFloat(faker.finance.amount({ min: 100, max: 200, dec: 2 })),
+          close: parseFloat(faker.finance.amount({ min: 100, max: 200, dec: 2 })),
+          high: parseFloat(faker.finance.amount({ min: 200, max: 300, dec: 2 })),
+          low: parseFloat(faker.finance.amount({ min: 50, max: 100, dec: 2 })),
+          date: new Date(currentDate),
+        });
       }
-
-      mockData.push({
-        open: parseFloat(faker.finance.amount({ min: 100, max: 200, dec: 2 })),
-        close: parseFloat(faker.finance.amount({ min: 100, max: 200, dec: 2 })),
-        high: parseFloat(faker.finance.amount({ min: 200, max: 300, dec: 2 })),
-        low: parseFloat(faker.finance.amount({ min: 50, max: 100, dec: 2 })),
-        date: date,
-      });
+      currentDate.setDate(currentDate.getDate() + 1);
     }
     return mockData;
   }
 
-  private generateMonthlyMockData(): SymbolPriceInfo[] {
-    const startYear = 1990;
-    const currentDate = new Date();
-    const endYear = currentDate.getFullYear();
+  private generateMonthlyMockData(from: Date, to: Date): SymbolPriceInfo[] {
+    const startYear = from.getFullYear();
+    const endYear = to.getFullYear();
     const mockData: SymbolPriceInfo[] = [];
 
     for (let year = startYear; year <= endYear; year++) {
       for (let month = 0; month < 12; month++) {
-        if (year === currentDate.getFullYear() && month > currentDate.getMonth()) {
+        if (year === to.getFullYear() && month > to.getMonth()) {
           break;
         }
         const lastDayOfMonth = new Date(year, month + 1, 0);
