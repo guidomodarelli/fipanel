306babc4352d6b852859fe9142c8b8f6e80afbe5
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-21 03:27:40 +0000

    feat: update useSymbolPriceMonthly hook to support date range for price retrieval

--- src/hooks/useSymbolPriceMonthly.ts
+++ src/hooks/useSymbolPriceMonthly.ts
@@ -1,36 +1,35 @@
-import { getSymbolPriceMonthly } from '@/app/setup';
+import { caljs, getSymbolPriceMonthly } from '@/app/setup';
 import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
 
 interface SymbolPriceMonthlyProps {
   symbol: string;
+  from: Date;
+  to: Date;
 }
 
-export const useSymbolPriceMonthly = ({ symbol }: SymbolPriceMonthlyProps) => {
-  const { data, isLoading } = useQuery({
+export const useSymbolPriceMonthly = ({ symbol, from, to }: SymbolPriceMonthlyProps) => {
+  const { data: dataMonthly, isLoading: isLoadingMonthly } = useQuery({
     queryKey: ['symbolPriceMonthly'],
-    queryFn: () => getSymbolPriceMonthly(symbol),
+    queryFn: () => getSymbolPriceMonthly(symbol, from, to),
   });
-
-  const isDecember = (date: Date) => {
-    return date.getMonth() === 11;
-  };
 
   const isLastYear = (date: Date) => {
     return date.getFullYear() === new Date().getFullYear() - 1;
   };
 
-  const getTheLastAnnualPrice = () => {
-    return data?.find((price) => isLastYear(price.date) && isDecember(price.date));
-  };
+  const getAnnualPrices = useMemo(() => {
+    return dataMonthly?.filter((price) => caljs().isDecember(price.date));
+  }, [dataMonthly]);
 
-  const getTheLastPrice = () => {
-    return data?.at(0);
-  };
+  const getTheLastAnnualPrice = useMemo(() => {
+    return getAnnualPrices?.find((price) => isLastYear(price.date));
+  }, [getAnnualPrices]);
 
   return {
-    data,
+    dataMonthly,
     getTheLastAnnualPrice,
-    getTheLastPrice,
-    isLoading,
+    getAnnualPrices,
+    isLoading: isLoadingMonthly,
   };
 };
