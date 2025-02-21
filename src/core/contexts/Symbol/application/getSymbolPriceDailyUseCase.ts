7ea570325a834d1bb6296ae4b5491e578e462eca
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-21 03:18:46 +0000

    feat: enhance getSymbolPriceDaily and getSymbolPriceMonthly use cases to support date range validation

--- src/core/contexts/Symbol/application/getSymbolPriceDailyUseCase.ts
+++ src/core/contexts/Symbol/application/getSymbolPriceDailyUseCase.ts
@@ -1,5 +1,19 @@
+import { caljs } from '@/app/setup';
+import { DateRangeError } from '../../shared/date/domain/DateRangeError';
+import { DateRangeLimitError } from '../../shared/date/domain/DateRangeLimitError';
+import { UnitTypes } from '../../shared/date/domain/types';
 import { SymbolProvider } from '../domain/SymbolProvider';
 
-export const getSymbolPriceDailyUseCase = (symbolProvider: SymbolProvider) => (symbol: string) => {
-  return symbolProvider.getSymbolPriceDaily(symbol);
-};
+const MONTHS_LIMIT = 1;
+const UNIT_TYPE: UnitTypes = 'months';
+
+export const getSymbolPriceDailyUseCase =
+  (symbolProvider: SymbolProvider) => (symbol: string, from: Date, to: Date) => {
+    if (from >= to) {
+      throw new DateRangeError();
+    }
+    if (caljs(to).diff(caljs(from), UNIT_TYPE) > MONTHS_LIMIT) {
+      throw new DateRangeLimitError(MONTHS_LIMIT, UNIT_TYPE);
+    }
+    return symbolProvider.getSymbolPriceDaily(symbol, from, to);
+  };
--- src/core/contexts/Symbol/application/getSymbolPriceMonthlyUseCase.ts
+++ src/core/contexts/Symbol/application/getSymbolPriceMonthlyUseCase.ts
@@ -1,5 +1,19 @@
+import { caljs } from "@/app/setup";
+import { DateRangeError } from '../../shared/date/domain/DateRangeError';
+import { DateRangeLimitError } from '../../shared/date/domain/DateRangeLimitError';
+import { UnitTypes } from "../../shared/date/domain/types";
 import { SymbolProvider } from '../domain/SymbolProvider';
 
-export const getSymbolPriceMonthlyUseCase = (symbolProvider: SymbolProvider) => (symbol: string) => {
-  return symbolProvider.getSymbolPriceMonthly(symbol);
-};
+const YEARS_LIMIT = 25;
+const UNIT_TYPE: UnitTypes = 'years';
+
+export const getSymbolPriceMonthlyUseCase =
+  (symbolProvider: SymbolProvider) => (symbol: string, from: Date, to: Date) => {
+    if (from >= to) {
+      throw new DateRangeError();
+    }
+    if (caljs(to).diff(caljs(from), UNIT_TYPE) > YEARS_LIMIT) {
+      throw new DateRangeLimitError(YEARS_LIMIT, UNIT_TYPE);
+    }
+    return symbolProvider.getSymbolPriceMonthly(symbol, from, to);
+  };
