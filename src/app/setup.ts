f09cbc2eb56390896c5cdbd64fd050ed405aa2fb
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-22 04:22:48 +0000

    refactor: update import statements to use type imports for better clarity and consistency

--- src/app/setup.ts
+++ src/app/setup.ts
@@ -1,28 +1,33 @@
 import { getDolarsPricesUseCase } from '@/core/contexts/Dolar/application/getDolarsPricesUseCase';
-import { DolarProvider } from '@/core/contexts/Dolar/domain/DolarProvider';
+import type { DolarProvider } from '@/core/contexts/Dolar/domain/DolarProvider';
 import { BluelyticsDolarProvider } from '@/core/contexts/Dolar/infrastructure/BluelyticsDolarProvider';
 import { getPrimaryFinancialMetricsUseCase } from '@/core/contexts/Metrics/application/getPrimaryFinancialMetricsUseCase';
-import { FinancialDataProvider } from '@/core/contexts/Metrics/domain/FinancialDataProvider';
+import type { FinancialDataProvider } from '@/core/contexts/Metrics/domain/FinancialDataProvider';
 import { BCRAFinancialDataProvider } from '@/core/contexts/Metrics/infrastructure/BCRAFinancialDataProvider';
-import { Calendar } from '@/core/contexts/shared/date/domain/Calendar';
-import { DayjsCalendar } from '@/core/contexts/shared/date/infrastructure/DayjsCalendar';
-import { HttpService } from '@/core/contexts/shared/http/domain/HttpService';
-import { FetchHttpService } from '@/core/contexts/shared/http/infrastructure/FetchHttpService';
-import { Logger } from '@/core/contexts/shared/logger/domain/Logger';
-import { ConsoleLogger } from '@/core/contexts/shared/logger/infrastructure/ConsoleLogger';
 import { getSymbolPriceDailyUseCase } from '@/core/contexts/Symbol/application/getSymbolPriceDailyUseCase';
 import { getSymbolPriceMonthlyUseCase } from '@/core/contexts/Symbol/application/getSymbolPriceMonthlyUseCase';
-import { SymbolProvider } from '@/core/contexts/Symbol/domain/SymbolProvider';
+import type { SymbolProvider } from '@/core/contexts/Symbol/domain/SymbolProvider';
 import { MockSymbolProvider } from '@/core/contexts/Symbol/infrastructure/MockSymbolProvider';
+import type { Calendar } from '@/core/contexts/shared/date/domain/Calendar';
+import { DayjsCalendar } from '@/core/contexts/shared/date/infrastructure/DayjsCalendar';
+import type { HttpService } from '@/core/contexts/shared/http/domain/HttpService';
+import { FetchHttpService } from '@/core/contexts/shared/http/infrastructure/FetchHttpService';
+import type { Logger } from '@/core/contexts/shared/logger/domain/Logger';
+import { ConsoleLogger } from '@/core/contexts/shared/logger/infrastructure/ConsoleLogger';
 
-export const caljs: (date?: Date) => Calendar = (date?: Date) => new DayjsCalendar(date);
+export const caljs: (date?: Date) => Calendar = (date?: Date) =>
+  new DayjsCalendar(date);
 export const _logger: Logger = new ConsoleLogger();
 const httpService: HttpService = new FetchHttpService();
-const bcraService: FinancialDataProvider = new BCRAFinancialDataProvider(httpService);
+const bcraService: FinancialDataProvider = new BCRAFinancialDataProvider(
+  httpService,
+);
 const dolarService: DolarProvider = new BluelyticsDolarProvider(httpService);
 const symbolService: SymbolProvider = new MockSymbolProvider();
 
-export const getPrimaryFinancialMetrics = getPrimaryFinancialMetricsUseCase(bcraService);
+export const getPrimaryFinancialMetrics =
+  getPrimaryFinancialMetricsUseCase(bcraService);
 export const getDolarsPrices = getDolarsPricesUseCase(dolarService);
-export const getSymbolPriceMonthly = getSymbolPriceMonthlyUseCase(symbolService);
+export const getSymbolPriceMonthly =
+  getSymbolPriceMonthlyUseCase(symbolService);
 export const getSymbolPriceDaily = getSymbolPriceDailyUseCase(symbolService);
