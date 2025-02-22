3ea8827f9f49a769547d32284961069f638ffa5a
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-22 03:58:46 +0000

    feat: refactor DateUtils to use object syntax and improve formatting in number utilities

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,15 +1,19 @@
 import { MONTH } from '@/core/contexts/shared/date/domain/types';
 
-export class DateUtils {
-  static isDecember(date: Date) {
+export const DateUtils = {
+  isDecember(date: Date) {
     return date.getMonth() === MONTH.DECEMBER;
-  }
+  },
 
-  static format(date: Date) {
-    return date.toLocaleDateString('es-AR', { day: 'numeric', month: 'numeric', year: 'numeric' });
-  }
+  format(date: Date) {
+    return date.toLocaleDateString('es-AR', {
+      day: 'numeric',
+      month: 'numeric',
+      year: 'numeric',
+    });
+  },
 
-  static isLastYear(date: Date) {
+  isLastYear(date: Date) {
     return date.getFullYear() === new Date().getFullYear() - 1;
-  }
-}
+  },
+};
