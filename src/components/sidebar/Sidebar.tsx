18b9f7fc71ee3841e745d19ec20602e2ef3ba8ef
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-22 23:51:44 +0000

    refactor: update Sidebar component to manage logo display based on sidebar state

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils';
 import { ChartBarIncreasingIcon, FileChartColumnIncreasingIcon, FilterIcon, HomeIcon } from 'lucide-react';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { Logo } from '../logo/Logo';
 import {
   Sidebar as SidebarBase,
@@ -84,7 +85,22 @@ const items: AppSidebarGroup[] = [
 ];
 
 export const Sidebar = () => {
-  const { open } = useSidebar();
+  const { open: isOpenSidebar } = useSidebar();
+  const [showLongLogo, setShowLongLogo] = useState(true);
+
+  useEffect(() => {
+    if (isOpenSidebar) {
+      // Al abrir, cambiar inmediatamente a logo largo
+      setShowLongLogo(true);
+      return () => {};
+    } else {
+      const timeout = setTimeout(() => {
+        setShowLongLogo(false);
+      }, 200);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [isOpenSidebar]);
 
   const renderGroup = (group: AppSidebarGroup) => {
     return (
@@ -119,10 +135,10 @@ export const Sidebar = () => {
       <SidebarHeader>
         <SidebarContent
           className={cn('flex flex-row', {
-            'justify-center': !open,
+            'justify-center': !showLongLogo,
           })}
         >
-          {open ? <Logo long /> : <Logo />}
+          {showLongLogo ? <Logo long /> : <Logo />}
         </SidebarContent>
       </SidebarHeader>
       {renderItems(items)}
