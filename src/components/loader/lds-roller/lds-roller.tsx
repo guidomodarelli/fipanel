3df9068d833468c6427e66a309a948f76e9697b0
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-22 04:25:20 +0000

    style: replace div tags with self-closing tags for cleaner JSX

--- src/components/loader/lds-roller/lds-roller.tsx
+++ src/components/loader/lds-roller/lds-roller.tsx
@@ -3,14 +3,14 @@ import './lds-roller.css';
 export const LdsLoader = () => {
   return (
     <div className='lds-roller'>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
     </div>
   );
 };
