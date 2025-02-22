44c2afb7d431f04e23927563971fe67ff1cbe288
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-22 23:51:39 +0000

    refactor: update Logo component to use utility class for conditional styling

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/lib/utils';
 import { CopyrightIcon } from 'lucide-react';
 
 interface LogoProps {
@@ -6,8 +7,8 @@ interface LogoProps {
 
 export const Logo = ({ long }: LogoProps) => {
   return (
-    <>
+    <div className={cn('flex', { 'ml-1 gap-1': long })}>
       <CopyrightIcon /> {long ? 'Inversiones' : ''}
-    </>
+    </div>
   );
 };
