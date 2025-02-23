aad85df678d103960059081785b6140aea10d0b7
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-23 01:01:22 +0000

    feat: refactor FinhubSymbolProvider to use a base URL for API requests

--- src/core/contexts/Symbol/infrastructure/Finhub/FinhubSymbolProvider.ts
+++ src/core/contexts/Symbol/infrastructure/Finhub/FinhubSymbolProvider.ts
@@ -3,6 +3,8 @@ import type { SymbolPriceInfo } from '../../domain/SymbolPriceInfo';
 import type { SymbolProvider } from '../../domain/SymbolProvider';
 import type { FinhubSymbolProviderResponse } from './FinhubSymbolProviderResponse';
 
+const BASE_URL = 'https://finnhub.io/api/v1';
+
 export class FinhubSymbolProvider implements SymbolProvider {
   constructor(private readonly httpService: HttpService) {}
 
@@ -10,10 +12,14 @@ export class FinhubSymbolProvider implements SymbolProvider {
     throw new Error('Method not implemented.');
   }
 
-  async getSymbolPriceMonthly(symbol: string): Promise<SymbolPriceInfo[]> {
-    const response = await this.httpService.get<FinhubSymbolProviderResponse>(
-      `https://finnhub.io/api/v1/stock/candle?token=${process.env.FINNHUB_API_KEY}&symbol=${symbol}`,
+  async get(path: string): Promise<FinhubSymbolProviderResponse> {
+    return this.httpService.get<FinhubSymbolProviderResponse>(
+      `${BASE_URL}${path}&token=${process.env.FINNHUB_API_KEY}`,
     );
+  }
+
+  async getSymbolPriceMonthly(symbol: string): Promise<SymbolPriceInfo[]> {
+    const response = await this.get(`stock/candle?symbol=${symbol}`);
 
     return [
       {
