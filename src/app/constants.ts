b3a23dcb32a2c1a588f0e7c7677437eeb782134b
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-23 22:40:35 +0000

    feat: add RSS feed fetching functionality with Clarin Economy RSS

---
+++ src/app/constants.ts
@@ -0,0 +1,2 @@
+const CLARIN_BASE_URL = 'https://www.clarin.com';
+export const CLARIN_ECONOMY_RSS = `${CLARIN_BASE_URL}/rss/economia`;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+'use client';
 import DolarCard from '@/components/cards/DolarCard/DolarCard';
 import IPCCard from '@/components/cards/IPCCard/IPCCard';
 import InterestRateCard from '@/components/cards/InterestRateCard/InterestRateCard';
 import { SIDEBAR_TRIGGER_HEIGHT } from '@/components/sidebar/SidebarLayout';
+import type { RssFeedMetadata } from '@/core/contexts/RSS-Reader/domain/RssDataStructure';
+import { useEffect, useState } from 'react';
+import { CLARIN_ECONOMY_RSS } from './constants';
+import { createLogger, rssReader } from './setup';
 
 export default function Home() {
+  const logger = createLogger(Home, 'enabled');
+  const [data, setData] = useState<Partial<RssFeedMetadata>>({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await rssReader.readFrom(CLARIN_ECONOMY_RSS);
+        setData(data);
+        logger.debug(data);
+      } catch (error) {
+        logger.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className={'flex flex-col gap-4 items-center'} style={{ height: `calc(100% - ${SIDEBAR_TRIGGER_HEIGHT})` }}>
       <div className='flex gap-4 flex-col md:flex-row'>
