066c0f8df9133801e53dc696115b763a3ec8d711
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-23 13:35:14 +0000

    refactor: update BluelyticsDolarProvider to use more specific response types for improved type safety

--- src/core/contexts/Dolar/infrastructure/BluelyticsDolarProvider.ts
+++ src/core/contexts/Dolar/infrastructure/BluelyticsDolarProvider.ts
@@ -2,14 +2,14 @@ import type { HttpService } from '../../shared/http/domain/HttpService';
 import type { DolarPriceMap } from '../domain/DolarPriceMap';
 import type { DolarProvider } from '../domain/DolarProvider';
 import type { FinancialValues } from '../domain/FinancialValues';
-import type { BluelyticsMoneyResponse } from './BluelyticsMoneyResponse';
+import type { BluelyticsDolarResponse, BluelyticsValueResponse } from './BluelyticsMoneyResponse';
 
 const BASE_URL = 'https://api.bluelytics.com.ar/v2';
 
 export class BluelyticsDolarProvider implements DolarProvider {
   constructor(private readonly httpService: HttpService) {}
 
-  private responseToDolars(money: BluelyticsMoneyResponse): FinancialValues {
+  private responseToDolars(money: BluelyticsValueResponse): FinancialValues {
     return {
       buy: money.value_buy,
       sell: money.value_sell,
@@ -17,12 +17,12 @@ export class BluelyticsDolarProvider implements DolarProvider {
     };
   }
 
-  async get(path: string) {
-    return this.httpService.get(`${BASE_URL}${path}`);
+  async get<T>(path: string): Promise<T> {
+    return this.httpService.get<T>(`${BASE_URL}${path}`);
   }
 
   async getDolarPrices(): Promise<DolarPriceMap> {
-    const response = await this.get('/latest');
+    const response = await this.get<BluelyticsDolarResponse>('/latest');
     return {
       oficial: this.responseToDolars(response.oficial),
       blue: this.responseToDolars(response.blue),
--- src/core/contexts/Dolar/infrastructure/BluelyticsMoneyResponse.ts
+++ src/core/contexts/Dolar/infrastructure/BluelyticsMoneyResponse.ts
@@ -1,5 +1,10 @@
-export interface BluelyticsMoneyResponse {
+export interface BluelyticsValueResponse {
   value_buy: number;
   value_sell: number;
   value_avg: number;
+}
+
+export interface BluelyticsDolarResponse {
+  oficial: BluelyticsValueResponse;
+  blue: BluelyticsValueResponse;
 }
