05f9a6bd80a679255c17b2b99f77b5568aa9dc13
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-24 23:46:33 +0000

    refactor: enhance HttpService methods to accept options for improved flexibility

--- src/core/contexts/shared/http/domain/HttpService.ts
+++ src/core/contexts/shared/http/domain/HttpService.ts
@@ -1,7 +1,11 @@
+export interface OptionsRequest extends RequestInit {
+  raw?: boolean;
+}
+
 export interface HttpService {
-  get<T = unknown>(url: string): Promise<T>;
-  post<T = unknown, D = unknown>(url: string, data: D): Promise<T>;
-  put<T = unknown, D = unknown>(url: string, data: D): Promise<T>;
-  patch<T = unknown, D = unknown>(url: string, data: D): Promise<T>;
-  delete<T = unknown>(url: string): Promise<T>;
+  get<T = unknown>(url: string, options?: OptionsRequest): Promise<T>;
+  post<T = unknown, D = unknown>(url: string, data: D, options?: OptionsRequest): Promise<T>;
+  put<T = unknown, D = unknown>(url: string, data: D, options?: OptionsRequest): Promise<T>;
+  patch<T = unknown, D = unknown>(url: string, data: D, options?: OptionsRequest): Promise<T>;
+  delete<T = unknown>(url: string, options?: OptionsRequest): Promise<T>;
 }
