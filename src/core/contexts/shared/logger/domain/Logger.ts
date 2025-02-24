16c1d8368bf8c772855286879b24a6a529e721ab
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-24 23:49:23 +0000

    refactor: simplify Logger interface by removing explicit message parameters

--- src/core/contexts/shared/logger/domain/Logger.ts
+++ src/core/contexts/shared/logger/domain/Logger.ts
@@ -1,10 +1,10 @@
 import type { Status } from './types';
 
 export interface Logger {
-  log(message: string, ...args: unknown[]): void;
-  debug(message: string, ...args: unknown[]): void;
-  info(message: string, ...args: unknown[]): void;
-  warn(message: string, ...args: unknown[]): void;
-  error(message: string, ...args: unknown[]): void;
-  getLogger(context: string[], status?: Status): Logger;
+  log(...args: unknown[]): void;
+  debug(...args: unknown[]): void;
+  info(...args: unknown[]): void;
+  warn(...args: unknown[]): void;
+  error(...args: unknown[]): void;
+  getLogger(context: unknown[], status?: Status): Logger;
 }
