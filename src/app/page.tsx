8c45eaf58a06e3aec8044a1425882e89c8d9a0d7
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-25 00:55:39 +0000

    Refactors RSS data fetching logic
    Moves RSS data fetching from Home component to RssFeedMetadata component
    Adds logger and URL as props to RssFeedMetadata for better separation of concerns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,30 +4,11 @@ import IPCCard from '@/components/cards/IPCCard/IPCCard';
 import InterestRateCard from '@/components/cards/InterestRateCard/InterestRateCard';
 import { RssFeedMetadata } from '@/components/rss/RssFeedMetadata';
 import { SIDEBAR_TRIGGER_HEIGHT } from '@/components/sidebar/SidebarLayout';
-import type { RssFeedMetadata as RssFeedMetadataType } from '@/core/contexts/RSS-Reader/domain/RssDataStructure';
-import { useEffect, useState } from 'react';
 import { CLARIN_ECONOMY_RSS } from './constants';
-import { createLogger, rssReader } from './setup';
+import { createLogger } from './setup';
 
 export default function Home() {
   const logger = createLogger(Home, 'enabled');
-  // @ts-expect-error
-  const [data, setData] = useState<RssFeedMetadataType>({});
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await rssReader.readFrom(`/api/rss?url=${CLARIN_ECONOMY_RSS}`);
-        setData(data);
-        logger.debug(data);
-      } catch (error) {
-        logger.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <div className={'flex flex-col gap-4 items-center'} style={{ height: `calc(100% - ${SIDEBAR_TRIGGER_HEIGHT})` }}>
       <div className='flex gap-4 flex-col md:flex-row'>
@@ -35,7 +16,7 @@ export default function Home() {
         <DolarCard />
         <InterestRateCard />
       </div>
-      {data && <RssFeedMetadata className='mt-6' {...data} />}
+      <RssFeedMetadata className='mt-6' logger={logger.getLogger(['RssFeedMetadata'])} url={CLARIN_ECONOMY_RSS} />
     </div>
   );
 }
--- src/components/rss/RssFeedMetadata.tsx
+++ src/components/rss/RssFeedMetadata.tsx
@@ -1,18 +1,40 @@
+'use client';
+import { rssReader } from '@/app/setup';
 import type { RssFeedMetadata as RssFeedMetadataType } from '@/core/contexts/RSS-Reader/domain/RssDataStructure';
+import type { Logger } from '@/core/contexts/shared/logger/domain/Logger';
 import type React from 'react';
+import { useEffect, useState } from 'react';
 import { RssFeedItem } from './RssFeedItem';
 
-interface RssFeedMetadataProps extends RssFeedMetadataType {
+interface RssFeedMetadataProps {
   className?: string;
+  url: string;
+  logger: Logger;
 }
 
-export const RssFeedMetadata: React.FC<RssFeedMetadataProps> = (props) => {
+export const RssFeedMetadata: React.FC<RssFeedMetadataProps> = ({ className, url, logger }) => {
+  const [data, setData] = useState({} as RssFeedMetadataType);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await rssReader.readFrom(`/api/rss?url=${url}`);
+        setData(data);
+        logger.debug(data);
+      } catch (error) {
+        logger.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <article className={props.className}>
-      <h1 className='text-2xl font-bold mb-4'>{props.title}</h1>
-      {props.item && (
+    <article className={className}>
+      <h1 className='text-2xl font-bold mb-4'>{data.title}</h1>
+      {data.item && (
         <div className='flex flex-col gap-4'>
-          {props.item.map((item) => (
+          {data.item.map((item) => (
             <RssFeedItem key={item.guid} {...item} />
           ))}
         </div>
