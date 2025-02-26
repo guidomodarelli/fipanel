7138787ce18bd5b3f7cade7bf57d09bc74b34d8a
Guido Modarelli <guidomodarelli25@gmail.com>
2025-02-26 23:38:57 +0000

    refactor: move Home component to its own file and simplify page structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,5 @@
-'use client';
-import DolarCard from '@/components/cards/DolarCard/DolarCard';
-import IPCCard from '@/components/cards/IPCCard/IPCCard';
-import InterestRateCard from '@/components/cards/InterestRateCard/InterestRateCard';
-import { RssFeedMetadata } from '@/components/rss/RssFeedMetadata';
-import { SIDEBAR_TRIGGER_HEIGHT } from '@/components/sidebar/SidebarLayout';
-import { CLARIN_ECONOMY_RSS } from './constants';
-import { createLogger } from './setup';
+import Home from '@/pages/Home';
 
-export default function Home() {
-  const logger = createLogger('Home', 'disabled');
-  return (
-    <div className={'flex flex-col gap-4 items-center'} style={{ height: `calc(100% - ${SIDEBAR_TRIGGER_HEIGHT})` }}>
-      <div className='flex gap-4 flex-col md:flex-row'>
-        <IPCCard />
-        <DolarCard />
-        <InterestRateCard />
-      </div>
-      <RssFeedMetadata className='mt-6' logger={logger.getLogger(['RssFeedMetadata'])} url={CLARIN_ECONOMY_RSS} />
-    </div>
-  );
+export default function page() {
+  return <Home />;
 }
