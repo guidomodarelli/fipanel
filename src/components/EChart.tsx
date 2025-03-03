f7f4370d35edad85fd7e9d6b1af4659bfa9a9758
Guido Modarelli <guidomodarelli25@gmail.com>
2025-03-03 18:59:12 +0000

    fix: refactor EChart component to improve resize handling and use constant for sidebar animation duration

--- src/components/EChart.tsx
+++ src/components/EChart.tsx
@@ -1,7 +1,7 @@
-import { useSidebar } from '@/components/ui/sidebar';
+import { SIDEBAR_ANIMATION_DURATION, useSidebar } from '@/components/ui/sidebar';
 import type { Logger } from '@/core/contexts/shared/logger/domain/Logger';
 import * as echarts from 'echarts';
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { Subject } from 'rxjs/internal/Subject';
 import { debounceTime } from 'rxjs/operators';
 
@@ -15,19 +15,25 @@ export const EChart: React.FC<EChartProps> = (props) => {
   const { open } = useSidebar();
   const container = useRef<HTMLDivElement>(null);
   const chart = useRef<echarts.ECharts>(null);
-  const resizeNotifier$ = new Subject<void>();
+  const resizeNotifier$ = useRef(new Subject<boolean>());
 
   useEffect(() => {
+    logger.debug('Initializing chart');
     chart.current = echarts.init(container.current, null, {
       renderer: 'canvas',
     });
 
     window.addEventListener('resize', emitResizeEvent);
-    resizeNotifier$.pipe(debounceTime(100)).subscribe(handleResize);
+    resizeNotifier$.current.pipe(debounceTime(SIDEBAR_ANIMATION_DURATION)).subscribe({
+      next: () => {
+        console.debug('Resizing chart');
+        chart.current?.resize();
+      },
+    });
 
     return () => {
       window.removeEventListener('resize', emitResizeEvent);
-      resizeNotifier$.complete();
+      resizeNotifier$.current.complete();
       chart.current?.dispose();
     };
   }, []);
@@ -36,17 +42,12 @@ export const EChart: React.FC<EChartProps> = (props) => {
     chart.current?.setOption(props.option);
   }, [props.option]);
 
-  const handleResize = useCallback(() => {
-    logger.debug('Resizing chart');
-    chart.current?.resize();
-  }, [logger]);
-
-  const emitResizeEvent = useCallback(() => {
-    resizeNotifier$.next();
-  }, []);
+  const emitResizeEvent = () => {
+    resizeNotifier$.current.next(true);
+  };
 
   useEffect(() => {
-    emitResizeEvent();
+    resizeNotifier$.current.next(true);
   }, [open]);
 
   return <div className='h-[33vh] w-full relative overflow-hidden' ref={container} />;
