e20f7ede3724ce9187fda0b347257b131fc72b15
Guido Modarelli <guidomodarelli25@gmail.com>
2025-03-05 00:31:33 +0000

    feat: add TradingViewFinancials component for displaying financial data

---
+++ src/components/embeddings/TradingView/TradingViewFinancials.tsx
@@ -0,0 +1,53 @@
+'use client';
+import type { ColorTheme } from '@/typings/ColorTheme';
+import React, { useEffect } from 'react';
+
+interface TradingViewFinancialsProps {
+  symbol: string;
+  locale?: string;
+  colorTheme?: ColorTheme;
+}
+
+// Fundamental Data Widget: https://www.tradingview.com/widget-docs/widgets/symbol-details/fundamental-data/
+const TradingViewFinancials: React.FC<TradingViewFinancialsProps> = ({
+  symbol,
+  locale = 'es',
+  colorTheme = 'light',
+}) => {
+  const tradingviewFinancialsRef = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-financials.js';
+    script.async = true;
+    script.innerHTML = JSON.stringify({
+      colorTheme,
+      displayMode: 'regular',
+      isTransparent: true,
+      locale,
+      showSymbolLogo: true,
+      symbol,
+      height: '100%',
+      width: '100%',
+    });
+
+    if (tradingviewFinancialsRef.current) {
+      tradingviewFinancialsRef.current.replaceWith(script);
+    }
+
+    return () => {
+      if (tradingviewFinancialsRef.current) {
+        tradingviewFinancialsRef.current.innerHTML = '';
+      }
+    };
+  }, []);
+
+  return (
+    <div className='h-[50rem]'>
+      <div ref={tradingviewFinancialsRef} />
+    </div>
+  );
+};
+
+export default TradingViewFinancials;
