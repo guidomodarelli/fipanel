d7695919a9848e40e4ae371a31c53b084ac1cd35
Guido Modarelli <guidomodarelli25@gmail.com>
2025-03-05 17:20:56 +0000

    Add TradingView symbol info component
    Implements a new TradingViewSymbolInfo component to embed TradingView's symbol info widget with configurable symbol, locale, and color theme.

---
+++ src/components/embeddings/TradingView/TradingViewSymbolInfo.tsx
@@ -0,0 +1,48 @@
+'use client';
+import type { ColorTheme } from '@/typings/ColorTheme';
+import React, { useEffect } from 'react';
+
+interface TradingViewSymbolInfoProps {
+  symbol: string;
+  locale?: string;
+  colorTheme?: ColorTheme;
+}
+
+// Symbol Info Widget: https://www.tradingview.com/widget-docs/widgets/symbol-details/symbol-info/
+const TradingViewSymbolInfo: React.FC<TradingViewSymbolInfoProps> = ({
+  symbol,
+  locale = 'es',
+  colorTheme = 'light',
+}) => {
+  const tradingviewSymbolInfoRef = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-symbol-info.js';
+    script.async = true;
+    script.innerHTML = JSON.stringify({
+      colorTheme,
+      isTransparent: true,
+      locale,
+      symbol,
+      width: '100%',
+      autosize: true,
+    });
+
+    if (tradingviewSymbolInfoRef.current) {
+      tradingviewSymbolInfoRef.current.innerHTML = '';
+      tradingviewSymbolInfoRef.current.appendChild(script);
+    }
+
+    return () => {
+      if (tradingviewSymbolInfoRef.current) {
+        tradingviewSymbolInfoRef.current.innerHTML = '';
+      }
+    };
+  }, [symbol, locale, colorTheme]);
+
+  return <div ref={tradingviewSymbolInfoRef} />;
+};
+
+export default TradingViewSymbolInfo;
