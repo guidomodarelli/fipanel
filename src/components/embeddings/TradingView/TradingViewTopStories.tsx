69ed9b4ab1eca4790931612037772ba5f1a0f3ef
Guido Modarelli <guidomodarelli25@gmail.com>
2025-03-05 22:34:47 +0000

    feat: refactor TradingViewTopStories component to improve prop handling and default values

--- src/components/embeddings/TradingView/TradingViewTopStories.tsx
+++ src/components/embeddings/TradingView/TradingViewTopStories.tsx
@@ -28,18 +28,15 @@ interface TradingViewTopStoriesPropsSymbol {
 type TradingViewTopStoriesProps = {
   colorTheme?: ColorTheme;
   locale?: string;
-  market?: Market;
   displayMode?: DisplayMode;
 } & (TradingViewTopStoriesPropsAllSymbols | TradingViewTopStoriesPropsSymbol | TradingViewTopStoriesPropsMarket);
 
 // Top Stories Widget: https://www.tradingview.com/widget-docs/widgets/news/top-stories/
-const TradingViewTopStories: FC<TradingViewTopStoriesProps> = ({
-  feedMode = 'market',
-  colorTheme = 'light',
-  locale = 'en',
-  market = 'stock',
-  displayMode = 'regular',
-}) => {
+const TradingViewTopStories: FC<TradingViewTopStoriesProps> = (_props) => {
+  const props = { ..._props };
+  props.colorTheme = props.colorTheme || 'light';
+  props.locale = props.locale || 'es';
+  props.displayMode = props.displayMode || 'regular';
   const tradingviewTimelineRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -48,22 +45,15 @@ const TradingViewTopStories: FC<TradingViewTopStoriesProps> = ({
     script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-timeline.js';
     script.async = true;
     script.innerHTML = JSON.stringify({
-      feedMode,
       isTransparent: false,
-      displayMode,
       width: '100%',
-      height: '100%',
-      colorTheme,
-      locale,
-      market,
+      height: window.innerHeight,
+      ...props,
     });
 
     if (tradingviewTimelineRef.current) {
       tradingviewTimelineRef.current.innerHTML = '';
       tradingviewTimelineRef.current.appendChild(script);
-      tradingviewTimelineRef.current.style.minHeight = '650px';
-      tradingviewTimelineRef.current.style.height = '100%';
-      tradingviewTimelineRef.current.style.width = '100%';
     }
 
     return () => {
@@ -71,9 +61,9 @@ const TradingViewTopStories: FC<TradingViewTopStoriesProps> = ({
         tradingviewTimelineRef.current.innerHTML = '';
       }
     };
-  }, [feedMode, colorTheme, locale, market, displayMode]);
+  }, [JSON.stringify(props), window.innerHeight]);
 
-  return <div ref={tradingviewTimelineRef} />;
+  return <div className='h-[50rem]' ref={tradingviewTimelineRef} />;
 };
 
 export default TradingViewTopStories;
