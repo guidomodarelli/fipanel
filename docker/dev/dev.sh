4ea32656bd5c11bd53d6415077cfeb969caa1c1e
Guido Modarelli <guidomodarelli25@gmail.com>
2025-03-12 22:54:31 +0000

    Moves FZF configuration to a separate script
    Relocates FZF default options from dev.sh to fzf.zsh for better organization and maintainability.

--- docker/dev/dev.sh
+++ docker/dev/dev.sh
@@ -2,7 +2,6 @@
 
 # Obtiene la raíz del repositorio con git
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
-FZF_DEFAULT_OPTS='--color=bg+:#293739,bg:#1B1D1E,border:#808080,spinner:#E6DB74,hl:#7E8E91,fg:#F8F8F2,header:#7E8E91,info:#A6E22E,pointer:#A6E22E,marker:#F92672,fg+:#F8F8F2,prompt:#F92672,hl+:#F92672 --ansi --cycle --border="rounded" --min-height=10 --height=30% --reverse --pointer="→"'
 
 source "$PROJECT_ROOT/scripts/styleText.zsh"
 source "$PROJECT_ROOT/scripts/fzf.zsh"
